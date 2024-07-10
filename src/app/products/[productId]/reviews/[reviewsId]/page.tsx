941d35c4eb9fd235d89ede611b1bdc9278af71d1
KrushnakantPavesoft <krushnakantshindepavesoft@gmail.com>
2024-07-10 15:53:39 +0530

    add not found page.

--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,8 +1,10 @@
+import { notFound } from "next/navigation";
 export default function ReviewsDetails({
   params,
 }: {
   params: { productId: string; reviewsId: string };
 }) {
+  if (parseInt(params.reviewsId) > 1000) return notFound();
   return (
     <h1>
       Details about {params.productId} product Reviews {params.reviewsId}
